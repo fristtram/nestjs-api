b3c14c17cd17ac7e3eb1841a633276fb6f3964e3
Fristtram Helder Fernandes <fristtram@gmail.com>
2022-04-02 13:59:52 -0300

    Criar documentação com Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  const config = new DocumentBuilder()
+    .setTitle('API nestJS')
+    .setDescription('Uma API piloto para os demais')
+    .setVersion('0.1')
+    .setContact(
+      'Kiria Web Technology',
+      'https://kiriaweb.com',
+      'fristtram@kiriaweb.com',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
